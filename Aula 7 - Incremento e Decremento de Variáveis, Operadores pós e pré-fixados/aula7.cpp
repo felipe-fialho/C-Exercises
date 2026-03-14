8d9c244f384ba816f66d1d369ab59ce2fa33be15
felipe-fialho <s.fransciscoxavier@gmail.com>
2026-03-14 19:58:14 -0300

    Add files via upload

---
+++ Aula 7 - Incremento e Decremento de Variáveis, Operadores pós e pré-fixados/aula7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	
+	//n1=n1+10;  n1+=10;
+	//n1=n1-10;  n1-=10;
+	//n1=n1*10;  n1*=10;
+	//n1=n1/2;   n1/=2;]
+	
+	//n1++; n1--;
+	
+	//n1++; Pos inclemento
+  
+	//++n1; Pré Inclemento
+	
+	
+	int n1=10;
+	
+	
+	cout << n1++ << "\n\n";
+	cout << n1 << "\n\n";
+
+	cout << --n1 << "\n\n";
+		
+	return 0;
+}
