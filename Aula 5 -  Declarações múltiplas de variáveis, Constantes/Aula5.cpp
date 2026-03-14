8fbb0e1ba91311de38fae5919eb04a4ee0d52593
felipe-fialho <s.fransciscoxavier@gmail.com>
2026-03-13 22:35:40 -0300

    Add files via upload

---
+++ Aula 5 -  Declarações múltiplas de variáveis, Constantes/Aula5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415 // valor fixo constante, que quando preecisar usala no codigo basta chamar está constante
+#define nome cout << "Felipe Fialho Ferreira\n\n"; // define tb pode ser usado para associar a um comando.
+
+int main()
+{
+	
+	int vidas = 3, tiros = 500, life = 100; // Declarar variaveis em somente em uma linha. Detalhe qualquer tipo
+	
+	/*int vida; // Declarar em linhas unicas
+	int tiros;
+	int life;
+	
+	int vidas; int tiros; int life; // Outrs forma */
+
+	cout << pi << "\n\n"; // define pi
+	
+	nome // define nome 
+
+	return 0;
+}
