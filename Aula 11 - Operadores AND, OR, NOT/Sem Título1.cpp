e35a4809e2db14b1de575200836e686c6ce48d31
felipe-fialho <s.fransciscoxavier@gmail.com>
2026-03-17 13:20:42 -0300

    Add files via upload

---
+++ Aula 11 - Operadores AND, OR, NOT/Sem Título1.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+
+	int num;
+	
+	/*
+	1 = Verdadeiro 
+	0 = Falso
+	
+	if(num == 1) / if(num)
+	if(num == 0  / if(!num)
+	if(num != 1) / if(!num)
+	*/
+
+	num = 0;
+	
+	if (!num){
+		cout << "\nVou ao Clube\n";
+	}else{
+		cout << "\nVou ao Cinema";
+	}
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+	/*
+	And. &&
+	Or
+	1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+	> 4 e < 7 And
+	<3 ou > 8 Or
+	3 a 6 
+	10 a 14
+	16 a 19
+	! Not Inverte o valor em um teste
+	*/
+
+
+
+// And && apresenta todas as sentenças verdaderas para ser true
+// Or || Apresenta so uma ou todas sentaças verdadeira pas ser true
+
+	//int num1;
+	//int num2;
+	
+
+	/*cout << "Digite dois numeros: ";
+	cin >> num1 >> num2;
+
+	
+  if ((num1 > 3 or num1 > 8) or (num2 < 3 or num2 < 8)) {
+        cout << "\nValor Aceito\n";
+    } else {
+        cout << "\nNao Aceito";
+    }*/
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
