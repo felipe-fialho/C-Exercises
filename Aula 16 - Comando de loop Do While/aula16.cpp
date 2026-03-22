8c3ba83ac9c57403c77b191d74c94f63f66f7297
felipe-fialho <s.fransciscoxavier@gmail.com>
2026-03-22 00:22:26 -0300

    Add files via upload

---
+++ Aula 16 - Comando de loop Do While/aula16.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+	int cont;
+	
+	cont = 20;
+	
+	do {
+		cout << "Faz o L - " << cont << "\n";
+		cont++;
+	} while(cont < 20);
+	
+	cout << "\nRotina Finalizada\n";
+		
+	
+	
+	
+	return 0;
+}
