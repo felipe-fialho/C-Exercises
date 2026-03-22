e63dbc763cfd86e4a8948c7a7caf150a347db014
felipe-fialho <s.fransciscoxavier@gmail.com>
2026-03-22 00:22:41 -0300

    Add files via upload

---
+++ Aula 17 - Comando de loop FOR/aula17.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*
+		for(ini ; condicional ; inclemento ou declemento){
+			//comando1;
+			//comando2;
+			//comando...;
+		}
+	*/
+	
+	int x, y, z;
+	/*
+	for(x=0, y=1, z=0 ; x <= 10 && z <= 6 ; x++, y+=2, z+=2 ){
+		cout << x << " - " ;
+		cout << y << " - "; 
+		cout << z <<  "\n";		
+	}*/
+	
+	//for(int tmp=0; tmp < 1000000000 ; tmp++);
+	
+	
+	
+		for(x=0; x <= 10  ; x++){
+		cout << x << "\n" ;
+
+	}
+	
+	
+	
+	return 0;
+}
